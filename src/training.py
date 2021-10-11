4765ea49c0951b000068ba4de7a9715dc7d9b184
mypython123 <mypython.pradhan@gmail.com>
2021-10-11 13:04:33 +0530

    10-11 Working

--- src/training.py
+++ src/training.py
@@ -1,10 +1,11 @@
-from src.utils.common import read_config
-from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from utils.common import read_config
+from utils.data_mgmt import get_data
+from utils.model import create_model
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
+    print(config)
     
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train),(X_valid, y_valid),(X_test,y_test) = get_data(validation_datasize)
